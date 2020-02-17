82df059c88f8f6e96ddd89dde3dc8cd8c0227294
Al-Amin Talukdar <md.talukdar@bkash.com>
2020-02-17 09:33:36 +0600

    eks module added

---
+++ modules/eks/output.tf
@@ -0,0 +1,9 @@
+output "eks_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+output "eks_certificate_authority" {
+  value = aws_eks_cluster.eks.certificate_authority
+}
+output "cluster_name" {
+  value = aws_eks_cluster.eks.name
+}
